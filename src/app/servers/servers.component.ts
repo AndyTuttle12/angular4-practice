f0406e5f4689a6c2b99b8bc838243b4b6ab70e5b
Andy Tuttle <andytuttle12@gmail.com>
2017-04-29 23:44:02 -0400

    Started learning more about directives and built-in ng-template.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreated = false;
   serverCreateStatus = 'No server was created.';
   serverName = '';
+  servers = ['Testserver', 'Testserver 2'];
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -19,6 +22,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreateStatus = 'Server was created! Name is ' + this.serverName;
   }
 
